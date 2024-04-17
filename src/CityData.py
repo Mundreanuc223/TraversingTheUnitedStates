f0e279ecf6ae66dfc98f650cb1b9c05a0018c6e1
yoaaronw <117215469+yoaaronw@users.noreply.github.com>
2024-04-17 11:59:42 -0400

    Added city attributes

--- src/CityData.py
+++ src/CityData.py
@@ -2,9 +2,14 @@ import csv
 
 class City:
 
-    def __init__(self, name, population):
+    def __init__(self, name, state, population, mean_household_inc, area_water, area_land, latitude):
         self.name = name
+        self.state = state
         self.population = population
+        self.mean_household_inc = mean_household_inc
+        self.area_water = area_water
+        self.area_land = area_land
+        self.latitude = latitude
 
     def readCSV(self, file):
         with open(file, 'r') as csvFile:
